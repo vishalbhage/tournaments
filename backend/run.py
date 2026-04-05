ca38198d7814f1a7d9a95d344938b8c84f9534b5
vishalbhage <vishalbhage.i.2.o@gmail.com>
2026-04-05 15:43:00 +0530

    Add files via upload

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,14 @@
-from app import create_app, db
+import os
+from app import create_app
+from app.extensions import db
+from app.seed import run_seed
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
+    run_seed()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=500, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
